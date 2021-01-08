4ef77068e2710580b019048fee33ceae91cdbf4f
Erik Mannsverk <58791192+erikmannsverk@users.noreply.github.com>
2021-01-08 11:36:43 +0100

    Oppgave 2 og 3 uke 1

---
+++ Uke_1/02_Personinfo.java
@@ -0,0 +1,10 @@
+class Personinfo {
+    public static void main(String[] args) {
+        String mittNavn = "Erik";
+        int telefonNummer = 3213413;
+        String gateAdr = "Humlevegen";
+        System.out.println(mittNavn);
+        System.out.println(telefonNummer);
+        System.out.println(gateAdr);
+    }
+}
---
+++ Uke_1/03_SumHeltall.java
@@ -0,0 +1,9 @@
+class SumHeltall {
+    public static void main(String[] args) {
+        int a = 4;
+        int b = 5;
+        int c;
+        c = a + b;
+        System.out.println("Summen er: " + c);
+    }
+}
