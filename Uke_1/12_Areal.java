5dcb6ab1c58da99df656337a97b0744f30c0a795
Erik Mannsverk <58791192+erikmannsverk@users.noreply.github.com>
2021-01-13 12:53:34 +0100

    Add files via upload

---
+++ Uke_1/12_Areal.java
@@ -0,0 +1,13 @@
+class Areal {
+    public static void main (String[] args) {
+        int bredde = 10;
+        int lengde = 20;
+        int areal;
+
+        System.out.println("Bredde: " + bredde);
+        System.out.println("Lengde: " + lengde);
+
+        areal = bredde * lengde;
+        System.out.println("Areal: " + areal);
+    }
+}
