76791edfe0451de6d52e5330388c8fc8f5de2a3e
Erik Mannsverk <58791192+erikmannsverk@users.noreply.github.com>
2021-01-14 10:50:26 +0100

    Add files via upload

---
+++ Uke_1/14_lokker.java
@@ -0,0 +1,9 @@
+class Lokker {
+    public static void main (String[] args){
+        int teller = 0;
+        while (teller < 10) {
+            System.out.println(teller);
+            teller ++;
+        }
+    }
+}
