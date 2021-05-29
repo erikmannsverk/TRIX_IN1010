cd109724864d64caeddcbafd1e32591dc144d9bd
Erik Mannsverk <58791192+erikmannsverk@users.noreply.github.com>
2021-05-29 12:19:42 +0200

    Oppgave 1_ Uke 4

---
+++ Uke_4/01_Arv/A.java
@@ -0,0 +1,13 @@
+class A {
+
+    void skrivKlasse(){
+        System.out.println("A");
+    }
+}
+
+class B extends A {
+
+    //void skrivKlasse(){
+        //System.out.println("B");
+    //}
+}
---
+++ Uke_4/01_Arv/Test.java
@@ -0,0 +1,10 @@
+class Test {
+    public static void main(String[] args){
+
+        B en = new B();
+        A to = en;
+
+        en.skrivKlasse();
+        to.skrivKlasse();
+    }
+}
